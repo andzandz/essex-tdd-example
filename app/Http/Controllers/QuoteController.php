dde122db6429d5727cb651a01a7b900163bea2c4
Andy C. Knight <andzandz@users.noreply.github.com>
2017-07-16 23:19:19 +0000

    Anti-gnomes are now disallowed (tests green)

--- app/Http/Controllers/QuoteController.php
+++ app/Http/Controllers/QuoteController.php
@@ -16,9 +16,10 @@ class QuoteController extends Controller
     public function calculate(Request $request)
     {
         $this->validate($request, [
-            'num_gnomes' => 'numeric|nullable',
+            'num_gnomes' => 'numeric|min:0|nullable',
         ], [
-            'num_gnomes.numeric' => 'The number of gnomes must be a number'
+            'num_gnomes.numeric' => 'The number of gnomes must be a number',
+            'num_gnomes.min' => 'Anti-gnomes are not allowed'
         ]);
 
         $quote_amount = 25 + 5 * $request->get('num_gnomes') + 50 * $request->get('chocolate_fountains');
